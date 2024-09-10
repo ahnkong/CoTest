9452a4e8f0f593f59b25b4d7fdbb575444ab6ec6
ahnkong <ahnkong92@gmail.com>
2024-09-10 10:41:21 +0900

    lv1 하루의 기적

---
+++ Test/src/algorithm/메모제이션.java
@@ -0,0 +1,36 @@
+package algorithm;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class 메모제이션 {
+	private static Map<Integer, Integer> memo = new HashMap<>();
+
+	
+	//--------------------------------
+	public static void main(String[] args) {
+		
+	int n = 10;
+	System.out.println("Fibonacci(" + n + ") = " + fibonacci(n));
+	
+	}
+	
+	
+	//------------------------------------
+	public static int fibonacci(int n) {
+		if(n == 0) return 0;
+		if(n == 1) return 1;
+		
+		//이미 계산된 값이 있으면 그 값을 반환
+		if(memo.containsKey(n)) {
+			return memo.get(n);
+		}
+	
+		//계산한 값을 메모에 저장
+		int result = fibonacci(n-1) + fibonacci(n-2);
+		memo.put(n, result);
+		
+		
+		return result;
+	}	
+}
