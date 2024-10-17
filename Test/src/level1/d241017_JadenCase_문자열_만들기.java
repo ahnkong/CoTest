f3ecf512489dd5f38bf6c194798c99e4e74e5f2a
ahnkong <ahnkong92@gmail.com>
2024-10-18 03:11:20 +0900

    2차원배열에서 Hash로 키와 밸류 저장한 후 비교하면서, 값 연산해주기 성공

--- Test/src/level1/d241017_JadenCase_문자열_만들기.java
+++ Test/src/level1/d241017_JadenCase_문자열_만들기.java
@@ -6,17 +6,20 @@ public class d241017_JadenCase_문자열_만들기 {
 	public static void main(String[] args) {
 
 		// 테스트 코드1
-		String s = " people unFollowed m0e";
+//		String s = "people unFollowed m0e";
 		// 테스트 코드2
 //		String s = "for the last week";
+		// 테스트 코드3
+		String s = "  for the what 1what  ";
 
 		String answer = "";
 
-		String[] words = s.split(" ");// 공백기준으로 나누기
-
-		System.out.println(Arrays.toString(words));
-		for (int i = 0; i < s.length(); i++) {
+//		String[] words = s.split(" ");// 공백기준으로 나누기
+//		System.out.println(Arrays.toString(words));
+		
+		for(int i = 0; i < s.length(); i++) {
 			char c = s.charAt(i);
+			
 			// 첫 문자는 무조건 대문자로 변환
 			if (i == 0 || s.charAt(i - 1) == ' ') {
 				answer += Character.toUpperCase(c);
@@ -26,7 +29,11 @@ public class d241017_JadenCase_문자열_만들기 {
 				answer += Character.toLowerCase(c);
 			}
 		}
-		answer = answer.trim();
+//		System.out.println("트림 전 : " + answer);
+//		answer = answer.trim();
+//		System.out.println("트림 후 : " + answer);
+		System.out.println();
+		
 		System.out.println(answer);
 	}
 }
