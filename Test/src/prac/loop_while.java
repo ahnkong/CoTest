cc953b7c93d9288e7a15502c512339d745b95126
ahnkong <ahnkong92@gmail.com>
2024-11-02 12:27:58 +0900

    while문과 java기본 개념들 다시 한번 복습

---
+++ Test/src/prac/loop_while.java
@@ -0,0 +1,29 @@
+package prac;
+
+public class loop_while {
+	public static void main(String[] args) {
+//		int sum = 0;
+//		
+//		int i = 1; // 초기식
+//		while(i<=100) { //반복문
+//			sum += 2; 
+//			i++; // 증감식
+//		}
+//		
+//		System.out.println(sum);
+		
+		
+		//while 이중 반복으로 돌리기
+		//초기식
+		int j = 0;
+		int i = 1;
+		
+		// 반복문
+		while(i<10) { // 조건문
+			System.out.println("현재 i : " + i ); // 루프!
+			j= 7*1;
+			System.out.println(" j : " + j );
+			i++; 
+		}
+	}
+}
